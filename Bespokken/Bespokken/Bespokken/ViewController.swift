c67513bfe655b0fcbc0ae94531e52772cc4dc74f
nickvido <nicholas.vidovich@gmail.com>
2015-10-10 10:53:21 -0400

    Basic app with icon that runs on the iPad

---
+++ Bespokken/Bespokken/Bespokken/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bespokken
+//
+//  Created by nickvido on 10/10/15.
+//  Copyright © 2015 nickvido. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
