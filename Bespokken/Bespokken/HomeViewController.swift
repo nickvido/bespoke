912b28fc6fc64cb297d8b6ed52887627e3ade022
nickvido <nicholas.vidovich@gmail.com>
2015-10-17 13:43:25 -0400

    Added btnImmediateModeToggle - updated layout to not go under top bar

--- Bespokken/Bespokken/HomeViewController.swift
+++ Bespokken/Bespokken/HomeViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    var buttons = [UIButton]()
+    
+    @IBOutlet weak var btnImmediateModeToggle: UIButton!
+    
+    
+    
+    @IBAction func onImmediateButtonTapped(sender: AnyObject) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+        buttons += [btnImmediateModeToggle]
+        
+        for button in buttons {
+            button.backgroundColor = UIColor.clearColor()
+            button.layer.borderColor = UIColor(red:0, green: 0, blue:0, alpha: 0.3).CGColor
+            button.layer.borderWidth = 1
+            button.layer.cornerRadius = 5
+            button.titleLabel?.textAlignment = .Center
+        }
     }
     
     override func didReceiveMemoryWarning() {
